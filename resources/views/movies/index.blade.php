ada909c5950d8c3589d6fcbfe20c300f9b88ed03
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-03-17 21:20:35 +0100

    Fixed star in Card movie

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -17,10 +17,13 @@
                     <address>- {{$movie->nationality}} -</address>
                     <div> {{date("M d Y", strtotime($movie->date))}}</div>
                     <div>
-                        @for ($i = 1; $i <= floor($movie->vote); $i++)
+                        @for ($i = 1; $i <= floor($movie->vote/2); $i++)
                         <i class="fas fa-star"></i>   
                         @endfor
-                        
+                        @for ($i = 1; $i <= floor(6 - ($movie->vote/2)); $i++)
+                        <i class="far fa-star"></i>   
+                        @endfor
+
                     </div>
                 </div>
             </div>
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -11,7 +11,14 @@
                 <h5>{{$movie->original_title}}</h5>
                 <address>- {{$movie->nationality}} -</address>
                 <div> {{date("M d Y", strtotime($movie->date))}}</div>
-                <div>{{$movie->vote}}</div>
+                <div>
+                    @for ($i = 1; $i <= floor($movie->vote/2); $i++)
+                        <i class="fas fa-star"></i>   
+                    @endfor
+                    @for ($i = 1; $i <= floor(6 - ($movie->vote/2)); $i++)
+                        <i class="far fa-star"></i>   
+                        @endfor
+                </div>
             </div>
         </div>
 </div>
